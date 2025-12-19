f9750a2b6a5dc1267dd3823923e31d2e1a1d6aa5
Venkat Aditya Vellanki <143627172+venkatavellanki@users.noreply.github.com>
2025-12-19 21:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
+    document.querySelector(link.getAttribute('href'))
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
